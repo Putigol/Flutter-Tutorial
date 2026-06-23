a87f8cdae0d6421952412252b05da7505c363653
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-23 16:44:30 +0700

    Basic operators and operations

---
+++ baitap_01/vidu_03.dart
@@ -0,0 +1,27 @@
+void main() {
+  //Các phép tính cơ bản
+  int tongSo = 2 + 2;
+  int hieuSo = 2 - 2;
+  int tichSo = 2 * 2;
+  double thuongSo = 2 / 2;
+
+  print(tongSo);
+  print(hieuSo);
+  print(tichSo);
+  print(thuongSo);
+
+  int layPhanDu = 7 % 2;
+  print(layPhanDu);
+
+  //Phép chia lấy phần nguyên
+  int layPhanNguyen = 7 ~/ 2;
+  print(layPhanNguyen);
+
+  //Phép gán và tính toán kết hợp
+  int x = 10;
+  x += 5;
+  x -= 2;
+  x *= 2;
+  x ~/= 2;
+  print(x);
+}
