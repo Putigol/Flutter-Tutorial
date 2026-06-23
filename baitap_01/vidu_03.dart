2984c93d19f1eb681ae575430b2728f51add391a
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-23 22:45:16 +0700

    Comparison operators

--- baitap_01/vidu_03.dart
+++ baitap_01/vidu_03.dart
@@ -1,9 +1,8 @@
 void main() {
-  //Prefix và Postfix
-  int a = 10;
-  int b = ++a; //b = 11, a = 11
-  int c = a++; //c = 11, a = 12
-  int d = --a; //d = 11, a = 11
-  int e = a--; //e = 11, a = 10
-  print('$a, $b, $c, $d, $e');
+  print(2 == 2);
+  print(2 != 3);
+  print(2 > 2);
+  print(2 < 2);
+  print(2 >= 3);
+  print(2 <= 3);
 }
