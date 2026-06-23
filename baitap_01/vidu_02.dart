1b5c3369e6b19e298ab79d5a97145e50deb1c711
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-23 15:33:57 +0700

    datatype

---
+++ baitap_01/vidu_02.dart
@@ -0,0 +1,19 @@
+/*Cách khai báo biến:
+  -Sử dụng "var" để Dart tự suy luận kiểu.
+  -Khai báo với kiểu cụ thể.
+  -Khai báo bằng kiểu object.
+ */
+
+void main() {
+  //Tự suy luận kiểu
+  var ten = "Thịnh"; //String
+  var tuoi = 20; //int
+
+  //Khai báo với kiểu dữ liệu cụ thể
+  String name = "Nguyen Van A";
+  int age = 20;
+
+  //Khai báo bằng kiểu object
+  Object obj = "Nguyen Van A";
+  print(obj);
+}
