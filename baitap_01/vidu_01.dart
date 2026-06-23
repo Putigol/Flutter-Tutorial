dc9c14bb6818f7696b30cdac2b820110c5cc4fab
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-23 15:02:13 +0700

    Introduction to Dart

---
+++ baitap_01/vidu_01.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = 'Nguyen Van A';
+  String address = 'Ha Noi';
+  int age = 20;
+  if (age >= 18) {
+    print('Ten: $name');
+    print('Tuoi: $age');
+    print('Dia chi: $address');
+  }
+}
