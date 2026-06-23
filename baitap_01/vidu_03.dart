627fa0ec26187c4421396e51a677991685da6880
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-23 22:58:50 +0700

    Operator ??=

--- baitap_01/vidu_03.dart
+++ baitap_01/vidu_03.dart
@@ -1,16 +1,9 @@
+//??=: sẽ gán giá trị nếu biến đang null
 void main() {
-  Object obj = "Hello World";
-  //Kiểm tra obj có phải là chuỗi hay không
-  if (obj is String) print(obj.length);
-
-  //Kiểm tra obj không phải kiểu int
-  if (obj is! int) print(obj);
-
-  Object obj2 = 10;
-  //Kiểm tra obj2 có phải kieu int hay khong
-  if (obj2 is int) print(obj2);
-
-  //Ép kiểu
-  String str = obj as String;
-  print(str.toUpperCase());
+  int? b;
+  b = null;
+  b ??= 10;
+  print('b=$b');
+  b ??= 20;
+  print('b=$b');
 }
