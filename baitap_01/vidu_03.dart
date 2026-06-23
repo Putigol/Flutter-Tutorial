60f009d5583e83fae404be64069fb26aa3dfcfe2
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-23 22:54:45 +0700

    Checking operators

--- baitap_01/vidu_03.dart
+++ baitap_01/vidu_03.dart
@@ -1,8 +1,16 @@
 void main() {
-  print(2 == 2);
-  print(2 != 3);
-  print(2 > 2);
-  print(2 < 2);
-  print(2 >= 3);
-  print(2 <= 3);
+  Object obj = "Hello World";
+  //Kiểm tra obj có phải là chuỗi hay không
+  if (obj is String) print(obj.length);
+
+  //Kiểm tra obj không phải kiểu int
+  if (obj is! int) print(obj);
+
+  Object obj2 = 10;
+  //Kiểm tra obj2 có phải kieu int hay khong
+  if (obj2 is int) print(obj2);
+
+  //Ép kiểu
+  String str = obj as String;
+  print(str.toUpperCase());
 }
