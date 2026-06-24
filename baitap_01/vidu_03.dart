f20cd57254462fee6c85303d4f11279f4fe74396
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-24 09:28:20 +0700

    ??=

--- baitap_01/vidu_03.dart
+++ baitap_01/vidu_03.dart
@@ -1,9 +1,16 @@
-//??=: sẽ gán giá trị nếu biến đang null
 void main() {
-  int? b;
-  b = null;
-  b ??= 10;
-  print('b=$b');
-  b ??= 20;
-  print('b=$b');
+  int diemToan = 7;
+  int diemVan = 8;
+  int diemTong = 0;
+  diemTong = diemToan + diemVan;
+  //Tính ĐTB
+  double diemTrungBinh = diemTong / 2;
+  //Gán điểm đạt/không đạt
+  String? ketQua;
+  ketQua ??= "Chưa xét";
+  if (diemTrungBinh >= 5) {
+    ketQua = "Đạt";
+  }
+  print('Điểm trung bình: $diemTrungBinh');
+  print('Kết quả: $ketQua');
 }
